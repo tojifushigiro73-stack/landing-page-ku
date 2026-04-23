55f9f6a18446fdf258f51e91bfab4d3360849ecd
Toji <tojifushigiro73@gmail.com>
2026-04-24 00:59:57 +0700

    push-3

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,11 +6,13 @@ import Scripts from "@/components/Scripts";
 const outfit = Outfit({
   variable: "--font-outfit",
   subsets: ["latin"],
+  display: 'swap',
 });
 
 const playfair = Playfair_Display({
   variable: "--font-playfair",
   subsets: ["latin"],
+  display: 'swap',
 });
 
 export const metadata = {
@@ -21,12 +23,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="id">
+    <html lang="id" className={`${outfit.variable} ${playfair.variable}`}>
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
       </head>
-      <body className={`${outfit.variable} ${playfair.variable}`}>
+      <body>
         <AppProvider>
           <Scripts />
           {children}
